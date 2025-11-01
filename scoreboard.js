46981fec0660a7d29b74bd54b7a7aa1accea5368
Jorege_xar <azureusgr@gmail.com>
2025-11-01 22:37:12 +0200

    fixed some CSS, started the script

---
+++ scoreboard.js
@@ -0,0 +1,16 @@
+// HOME TEAM
+const home_section = document.getElementById("homeTeam");
+const home_name = document.getElementById("homeName");
+const home_logo = document.getElementById("homeLogo");
+const home_score = document.getElementById("homeScore");
+const home_fouls = document.getElementById("homeFouls");
+
+// AWAY TEAM
+const away_section = document.getElementById("awayTeam");
+const away_name = document.getElementById("awayName");
+const away_logo = document.getElementById("awayLogo");
+const away_score = document.getElementById("awayScore");
+const away_fouls = document.getElementById("awayFouls");
+
+
+console.log(away_fouls);
