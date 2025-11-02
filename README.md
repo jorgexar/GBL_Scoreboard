c38114a78cdeb96789e6f1cdb5e4bf819808b3fd
Jorege_xar <azureusgr@gmail.com>
2025-11-02 18:43:22 +0200

    Some veery basic javascript experiment

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ##
 This is the reference\
 ![Screenshot of the original scoreboard used by the official GBL](img/original.jpg)
+
+# TODO
+1. Better way of displaying teams' logos
+2. Control Panel for Names,Logos,Score,Fouls
+3. Control Panel for Period, Period Timer, Attack Timer
+4. Animations
+5. Display players *Optional
+6. Control panel for Player stat *Optional
--- scoreboard.js
+++ scoreboard.js
@@ -2,15 +2,29 @@
 const home_section = document.getElementById("homeTeam");
 const home_name = document.getElementById("homeName");
 const home_logo = document.getElementById("homeLogo");
-const home_score = document.getElementById("homeScore");
+const home_score = document.getElementById("homeScore").firstChild;
 const home_fouls = document.getElementById("homeFouls");
 
 // AWAY TEAM
 const away_section = document.getElementById("awayTeam");
 const away_name = document.getElementById("awayName");
 const away_logo = document.getElementById("awayLogo");
-const away_score = document.getElementById("awayScore");
+const away_score = document.getElementById("awayScore").firstChild;
 const away_fouls = document.getElementById("awayFouls");
 
 
-console.log(away_fouls);
+function addScore(team, points) {
+    if (team == "H"){
+        team_score = home_score.innerText;
+        team_score = parseInt(team_score);
+        team_score += points;
+        home_score.innerText = team_score;
+        return 1;
+    }
+    if (team == "A"){
+        team_score = away_score.innerText;
+        team_score = parseInt(team_score);
+        team_score += points;
+        away_score.innerText = team_score;
+    }
+}
